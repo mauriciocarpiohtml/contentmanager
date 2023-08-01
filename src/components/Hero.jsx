0fc936f25c622d17ad851ee20e87c636c8bdb7e7
mauriciocarpiohtml <carpio.html@gmail.com>
2023-08-01 12:33:14 -0300

    Hero fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,15 @@
+import hero from '../img/bg-content.jpg'
 
 function Hero() {
+  const backgroundImageStyle = {
+    backgroundImage: `url(${hero})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+  };
   return (
-    <section className={'w-full h-[90vh] flex justify-center items-center bg-hero bg-center bg-cover relative'}>
+    <section 
+    className='w-full h-[90vh] flex justify-center items-center relative'
+    style={backgroundImageStyle}>
         <div className='absolute w-full h-full bg-black opacity-[68%]'></div>
         <div className=' md:w-[50%] text-center p-5 z-10'>
             <h2 className='text-3xl lg:text-4xl font-bold uppercase text-white tracking-tight'>Crea contenido por<br/> <span className='text-[#FFBE00]'>el que te paguen </span><br/></h2>
